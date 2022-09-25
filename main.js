f2913d3b5671b403e406d306102d895d7916d811
TechstaZack <114269271+TechstaZack@users.noreply.github.com>
2022-09-25 12:19:59 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function owner() {
+    window.alert("The owner is Zac, and his username is Techsta#8333. ");
+}
+
+function requirement() {
+    window.alert(" to enter teir 1, you need: Level 20 in the chosen game, Rank C+ or higher in Ranked/Anarchy Battles, 5 hours or more on the chosen game.");
+}
+
+function bots() {
+    window.alert("The bots are: Party beast, so you can make any VC any time, Dyno, to auto mod everyone, and tickettool, so we can help you whenever.");
+}
